edd5a2f8cc466e90b7ec34bbe8bf8f48c662baac
Razeen-Shaikh <razeen9796@outlook.com>
2025-03-03 20:36:03 +0530

    Add basic styling to these components using Tailwind CSS.

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import React from "react";
 import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
 import "reactflow/dist/style.css";
@@ -9,13 +8,7 @@ const nodes = [
   { id: "1", position: { x: 250, y: 0 }, data: { label: "Start" } },
   { id: "2", position: { x: 100, y: 150 }, data: { label: "Step 1" } },
   { id: "3", position: { x: 400, y: 150 }, data: { label: "Step 2" } },
-  { id: "4", position: { x: 250, y: 300 }, data: { label: "Step 3" } },
-  { id: "5", position: { x: 100, y: 450 }, data: { label: "Step 4" } },
-  { id: "6", position: { x: 395, y: 450 }, data: { label: "Step 5" } },
-  { id: "7", position: { x: 100, y: 600 }, data: { label: "Step 6" } },
-  { id: "8", position: { x: 270, y: 600 }, data: { label: "Step 7" } },
-  { id: "9", position: { x: 520, y: 600 }, data: { label: "Step 8" } },
-  { id: "10", position: { x: 250, y: 750 }, data: { label: "End" } },
+  { id: "4", position: { x: 250, y: 300 }, data: { label: "End" } },
 ];
 
 const edges = [
@@ -23,33 +16,21 @@ const edges = [
   { id: "e1-3", source: "1", target: "3" },
   { id: "e2-4", source: "2", target: "4" },
   { id: "e3-4", source: "3", target: "4" },
-  { id: "e4-5", source: "4", target: "5" },
-  { id: "e4-6", source: "4", target: "6" },
-  { id: "e5-7", source: "5", target: "7" },
-  { id: "e6-8", source: "6", target: "8" },
-  { id: "e6-9", source: "6", target: "9" },
-  { id: "e7-10", source: "7", target: "10" },
-  { id: "e8-10", source: "8", target: "10" },
-  { id: "e9-10", source: "9", target: "10" },
 ];
 
 const Roadmap: React.FC = () => {
   return (
-    <div className="h-[100vh] w-full bg-white shadow-md rounded-lg p-4">
-      <div>
-        <Link
-          href="/"
-          className="text-blue-500 hover:underline mb-4 absolute left-4"
-        >
-          Back
-        </Link>
-        <h2 className="text-xl font-bold text-center mb-4">Roadmap</h2>
+    <div className="max-h-[100vh] w-full bg-white shadow-lg rounded-lg p-6 border border-gray-200">
+      <h2 className="text-2xl font-bold text-center text-blue-600 mb-4">
+        📌 Roadmap
+      </h2>
+      <div className="h-[88vh] rounded-lg overflow-hidden border border-gray-300 bg-gray-50">
+        <ReactFlow nodes={nodes} edges={edges} fitView>
+          <Background />
+          <Controls />
+          <MiniMap />
+        </ReactFlow>
       </div>
-      <ReactFlow nodes={nodes} edges={edges} fitView>
-        <Background />
-        <Controls />
-        <MiniMap />
-      </ReactFlow>
     </div>
   );
 };
